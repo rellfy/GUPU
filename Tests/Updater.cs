e777f67fed787d38247506856baa8644d9debd81
Lorenzo Fontoura <lorenzo@nioel.com>
2021-02-21 21:14:58 +1000

    add test module

---
+++ Tests/Updater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace GUPU.Tests {
+    public class Updater {
+        [Test]
+        public void GetLastCommitHash_ValidRepoUrl_DoesNotThrow() {
+            string hash = GUPU.Updater.GetLastCommitHash(
+                "https://github.com/rellfy/rwasm.git"
+            );
+            Assert.Equals(hash.Length, 40);
+        }
+        
+        [Test]
+        public void GetLastCommitHash_InvalidRepoUrl_Throws() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                GUPU.Updater.GetLastCommitHash(
+                    "https://github.com/rellfy/thisrepodoesnotexistatall.git"
+                );
+            });
+        }
+    }
+}

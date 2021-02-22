5c80b5d0b20c1f8a87248d4e98d16aef48f162eb
Lorenzo Fontoura <lorenzo@nioel.com>
2021-02-22 23:14:24 +1000

    add PackageLock.cs

---
+++ Editor/PackageLock.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace GUPU {
+    public static class PackageLock {
+        public static Dictionary<string, Dependency> File {
+            get {
+                string json = System.IO.File.ReadAllText(GetPackageLockPath());
+                return JsonConvert.DeserializeObject<Object>(json).dependencies;
+            }
+            set {
+                Object lockObject = new Object();
+                lockObject.dependencies = value;
+                string json = JsonConvert.SerializeObject(lockObject, Formatting.Indented);
+                System.IO.File.WriteAllText(GetPackageLockPath(), json);
+                AssetDatabase.Refresh();
+            }
+        }
+        
+        public struct Object {
+            public Dictionary<string, Dependency> dependencies;
+        }
+    
+        public struct Dependency {
+            public string version;
+            public int depth;
+            public string source;
+            public Object dependencies;
+            public string hash;
+        } 
+        
+        private static string GetPackageLockPath() => (
+            System.IO.Path.GetFullPath(System.IO.Path.Combine(
+                Application.dataPath, "../Packages/packages-lock.json"
+            ))
+        );
+    }
+}
